4f46f650100f91044ada16696542b0a61b7b6c72
Zhicheng Wang <zcwang@thoughtworks.com>
2015-02-21 12:02:17 +0800

    实现“图形验证码”指令

---
+++ app/decorators/captcha.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('app').directive('bfCaptcha', function bfCaptcha() {
+  return {
+    restrict: 'A',
+    link: function (scope, element) {
+      var changeSrc = function() {
+        element.attr('src', '/api/captcha.jpg?random=' + new Date().getTime());
+      };
+      changeSrc();
+      element.on('click', function() {
+        changeSrc();
+      });
+    }
+  };
+});
