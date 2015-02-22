7dec512e4148e93a99817b35e0b0b51ef01a04c2
Zhicheng Wang <zcwang@thoughtworks.com>
2015-02-22 19:04:01 +0800

    tree service的单元测试：加上其他断言（红）

--- test/unit/services/utils/treeSpec.js
+++ test/unit/services/utils/treeSpec.js
@@ -57,4 +57,36 @@ describe("tree service >", function () {
     expect(findById(111).$isChecked()).toBeTruthy();
     expect(findById(112).$isChecked()).toBeTruthy();
   });
+  it('选中所有子节点时，自动选中父节点', function () {
+    findById(111).$check();
+    findById(112).$check();
+    expect(findById(11).$isChecked()).toBeTruthy();
+    findById(12).$check();
+    expect(findById(1).$isChecked()).toBeTruthy();
+  });
+  it('取消选中所有子节点时，自动取消选中父节点', function () {
+    // 先全都选中
+    findById(1).$check();
+    expect(findById(112).$isChecked()).toBeTruthy();
+    // 逐个反选
+    findById(111).$unCheck();
+    findById(112).$unCheck();
+    expect(findById(11).$isChecked()).toBeFalsy();
+    findById(12).$unCheck();
+    expect(findById(1).$isChecked()).toBeFalsy();
+  });
+  it('部分选中子节点时，父节点显示为“待定”状态', function () {
+    findById(111).$check();
+    expect(findById(11).$isIndeterminate()).toBeTruthy();
+    expect(findById(1).$isIndeterminate()).toBeTruthy();
+    // 子节点本身和没有子节点的节点不受影响
+    expect(findById(111).$isIndeterminate()).toBeFalsy();
+    expect(findById(12).$isIndeterminate()).toBeFalsy();
+  });
+  it('全部选中子节点时，父节点为“非待定”状态', function() {
+    findById(111).$check();
+    expect(findById(11).$isIndeterminate()).toBeTruthy();
+    findById(112).$check();
+    expect(findById(11).$isIndeterminate()).toBeFalsy();
+  });
 });
